df92857a278dce0761c37b76d176177ef1c5eded
Chloe Wildman <wildmanchloe@outlook.com>
2025-11-29 21:31:13 -0500

    test 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../css/styles.css"
 function Footer() {
     return (
         <footer>
-            <p>&copy; 2025 Teddy Crunch Co. | All rights reserved</p>
+            <p>&copy; 2025 Teddy Crunch Co. - All rights reserved</p>
             <hr></hr>
                 <a href="#top" className="toTop">Back to Top</a>
         </footer>
