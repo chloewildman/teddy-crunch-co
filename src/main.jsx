dbf3fc805cee2d979bbafa344c3b04b371337c9f
Chloe Wildman <wildmanchloe@outlook.com>
2025-11-29 21:16:26 -0500

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <App />
     </Router>
 )
